d4b6a7e053ecd9968d5b7968a3577d2e16a8e4b0
juliofdez <114910076+juliofdez@users.noreply.github.com>
2023-01-16 14:38:24 +0100

    Add files via upload

---
+++ EJ1_.js
@@ -0,0 +1,49 @@
+    // Crea una lista de números pares del 2 al 20 utilizando un bucle while.
+    var lista_pares = [];
+    var i = 0;
+    while(i<=20){
+        if(i%2==0){
+            lista_pares.push(i);
+        }
+        i = i +1;
+    }
+    console.log(lista_pares);
+
+    // Crea una lista de números impares del 1 al 19 utilizando un bucle for.
+    var lista_impares = [];
+    var i = 0;
+    for( var index=0 ; index<=19 ; i++){
+        if(i%2==1){
+            lista_impares.push(index);
+        }
+        i = i +1;
+    }
+    console.log(lista_impares);
+
+    
+    // Crea una lista de comida.
+    var lista_comida = ["tortilla","gambas","tetas de amor"]
+    console.log(lista_comida);
+    
+    // Agrega un elemento al principio de la lista de comida que creaste en el ejercicio anterior.
+    var lista_comida = ["tortilla","frijoles","gambas","tetas de amor"]
+    console.log(lista_comida);
+
+    // Accede al segundo elemento de la lista de comida y cambia su valor por otro diferente.
+    var lista_comida = ["espinacas","gambas","tetas de amor"]
+    console.log(lista_comida);
+
+    // Recorre la lista de comida utilizando un bucle while y muestra cada elemento en la consola.
+    
+    var lista_comida = ["tortilla","gambas","tetas de amor"]
+    console.log(lista_comida);
+
+    // Crea una función que reciba una lista como parámetro y devuelva el último elemento de la lista.
+    
+    // Crea una función que reciba una lista y un elemento como parámetros y agregue el elemento a la lista si no se encuentra en ella.
+    
+    // Crea una función que reciba una lista como parámetro y devuelva una nueva lista con los elementos de la lista original en orden inverso.
+    
+    // Crea una función que reciba una lista de números como parámetro y devuelva la suma de todos los números de la lista.
+    
+    // Crea una función que reciba una lista de números como parámetro y devuelva la media de todos los números de la lista.
